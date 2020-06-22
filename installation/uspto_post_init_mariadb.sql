8738147a37708569655460dded0e914ed06a92b1
Development <joseph.lee.esl@gmail.com>
2020-06-21 22:44:27 -0400

    METRICS queries built for citations

--- installation/uspto_post_init_mariadb.sql
+++ installation/uspto_post_init_mariadb.sql
@@ -11,27 +11,30 @@ CREATE TABLE IF NOT EXISTS uspto.METRICS_G (
   ENGINE = InnoDB;
 
 --
--- Method 1: Insert statement
+-- Create metrics Record for all GrantID
 --
-INSERT INTO uspto.METRICS_G (`GrantId`, `ForwardCitCnt`, `BackwardCitCnt`)
-SELECT a.GrantID, count(c.CitedID), count(b.GrantID)
-FROM uspto.GRANT as a
-JOIN uspto.GRACIT_G as b ON
-a.GrantID=b.GrantID
-JOIN uspto.GRACIT_G as c ON
-a.GrantID=c.CitedID
-GROUP BY a.GrantID;
-
---
--- Method 2: Update Statement
---
-
 INSERT INTO uspto.METRICS_G (`GrantID`) SELECT `GrantID` FROM uspto.GRANT;
-
-UPDATE uspto.METRICS_G, (select CitedID, count(*) as count from uspto.GRACIT_G group by GrantID) as t2
+--
+-- Calculate Forward Citations for all GrantID
+--
+UPDATE uspto.METRICS_G, (select CitedID, count(*) as count from uspto.GRACIT_G group by CitedID) as t2
 SET    uspto.METRICS_G.ForwardCitCnt = t2.count
 WHERE  uspto.METRICS_G.GrantID = t2.CitedID;
-
+--
+-- Set all patents without FWD citations to 0
+--
+UPDATE uspto.METRICS_G
+SET uspto.METRICS_G.ForwardCitCnt = 0
+WHERE uspto.METRICS_G.ForwardCitCnt is NULL;
+--
+-- Calculate Backward Citations for all GrantID
+--
 UPDATE uspto.METRICS_G, (select GrantID, count(*) as count from uspto.GRACIT_G group by GrantID) as t2
 SET    uspto.METRICS_G.BackwardCitCnt = t2.count
 WHERE  uspto.METRICS_G.GrantID = t2.GrantID;
+--
+-- Set all patents without BWD citations to 0
+--
+UPDATE uspto.METRICS_G
+SET uspto.METRICS_G.BackwardCitCnt = 0
+WHERE uspto.METRICS_G.BackwardCitCnt is NULL;
